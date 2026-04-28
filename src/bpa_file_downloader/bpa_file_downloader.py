b386c4b950890d422b7637c7f06cd39ce6e9660a
Tom Harrop <twharrop@gmail.com>
2026-04-29 08:26:44 +1000

    Fix: Handle base_urls without trailing slash

--- src/bpa_file_downloader/bpa_file_downloader.py
+++ src/bpa_file_downloader/bpa_file_downloader.py
@@ -3,7 +3,13 @@
 from snakemake_setup import get_snakefile, run_workflow
 from common import generate_parser, log_version
 from pathlib import Path
+from urllib import parse
 import os
+
+
+def check_url(url):
+    split_url = parse.urlsplit(url)
+    return parse.urlunsplit(split_url)
 
 
 def parse_arguments():
@@ -11,7 +17,7 @@ def parse_arguments():
 
     parser.add_argument(
         "bioplatforms_url",
-        type=str,
+        type=check_url,
     )
     parser.add_argument(
         "file_name",
@@ -27,13 +33,11 @@ def parse_arguments():
 
     settings_parser.add_argument(
         "--base_url",
-        type=str,
-        help=(
-            """
+        type=check_url,
+        help=("""
         The base_url to attempt downloading this file from the non-AWS Data
         Portal mirror. If provided, this will be tried before the main URL.
-        """
-        ),
+        """),
     )
 
     return parser.parse_args()
