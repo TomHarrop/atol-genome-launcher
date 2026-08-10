790d0ae511ec4bb7f8a874a0e41e16d9be4a199f
Tom Harrop <twharrop@gmail.com>
2026-08-10 16:57:17 +1000

    hit the qc_reads_report endpoint !wip

--- extras/deposit_and_broker_reads.py
+++ extras/deposit_and_broker_reads.py
@@ -4,7 +4,7 @@ import argparse
 import json
 from pathlib import Path
 
-from canopy_client import canopy_login
+from canopy_client import canopy_login, CanopySession
 from common import generate_parser
 from yaml_manifest import Manifest
 
@@ -31,16 +31,46 @@ def read_json_from_path(path_to_json_file: Path) -> dict[str, str | int]:
         return json.load(f)
 
 
+def post_qc_reads_report(
+    assembly_id: str,
+    body: dict[str, str | int],
+    canopy_session: CanopySession,
+    endpoint: str = "qc_reads_report",
+):
+    url_template = _endpoints.get(endpoint)
+    url_suffix = url_template.format(assembly_id=assembly_id)
+
+    # testing - are we logged in?
+    # response = canopy_session.get("/api/v1/samples/")
+    # print(response.json())
+
+    # testing - can we post?
+    # logout = canopy_session.post("/api/v1/auth/logout")
+    # logout.raise_for_status()
+    # raise ValueError(logout.content)
+
+    # works - right now we get a validation error
+    response = canopy_session.post(url=url_suffix, data=json.dumps(body))
+    response.raise_for_status()
+
+    raise ValueError(response.content)
+
+
 def main():
 
     args = parse_arguments()
 
     canopy_session = canopy_login()
 
+    # testing
+    # response = canopy_session.get("/api/v1/samples/")
+    # raise ValueError(response.content)
+
     qc_report_dict = read_json_from_path(args.qc_reads_report)
 
     with open(args.manifest, "rb") as f:
         manifest = Manifest.model_validate_json(f.read())
+    assembly_id = manifest.assembly_id
 
     # this will be passed to canopy
     # raise ValueError(manifest.assembly_id)
@@ -49,7 +79,16 @@ def main():
     package_reads = manifest.reads.get(args.bpa_package_id)
     checksum_values = package_reads.all_md5sums
 
+    post_qc_reads_report(
+        assembly_id=assembly_id, canopy_session=canopy_session, body=qc_report_dict
+    )
 
+
+# The trailing slash is important. It only works if you use the exact format on
+# the Swagger page. e.g. for assembly submission you have to POST to
+# /api/v1/assemblies/submission/ (trailing slash), but for QC report submission
+# its /api/v1/assemblies/{assembly_id}/qc-reads/report (no trailing slash).
+# Does it have something to do with the params?
 _endpoints = {"qc_reads_report": "/api/v1/assemblies/{assembly_id}/qc-reads/report"}
 
 
